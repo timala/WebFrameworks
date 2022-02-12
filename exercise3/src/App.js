24a8b537fd143767f10b507ecce9497bf181b00a
Tiina Lakkapää <t0lati02@students.oamk.fi>
2022-02-12 16:34:53 +0200

    exercise3

---
+++ exercise3/src/App.js
@@ -0,0 +1,72 @@
+import './App.css';
+import { useState } from 'react';
+import Products from './components/Products';
+import image1 from './components/kuva.png';
+import image2 from './components/kuva2.png';
+import image3 from './components/kuva3.png';
+import image4 from './components/kuva4.png';
+import image5 from './components/kuva5.png';
+import image6 from './components/kuva6.png';
+
+function App() {
+  const [products, setProducts] = useState([
+    {
+      id: 1, 
+      productImage: image1,
+      name: "HP 24mh FHD Monitor - Computer Monitor with 23.8-Inch IPS Display (1080p) - Built-In Speakers and VESA Mounting - Height/Tilt Adjustment for Ergonomic Viewing - HDMI and DisplayPort - (1D0J9AA#ABA)",
+      price: 226.99,
+      shipping: 48.63
+    },
+    {
+      id: 2, 
+      productImage: image2,
+      name: "Original HP 63XL Black High-yield Ink Cartridge | Works with HP DeskJet 1112, 2130, 3630 Series; HP ENVY 4510, 4520 Series; HP OfficeJet 3830, 4650, 5200 Series | Eligible for Instant Ink | F6U64AN",
+      price: 41.89,
+      shipping: 13.72
+    },
+    {
+      id: 3, 
+      productImage: image3,
+      name: "Roku Streaming Stick+ | HD/4K/HDR Streaming Device with Long-range Wireless and Roku Voice Remote with TV Controls",
+      price: 29.99,
+      shipping: 15.41
+    },
+    {
+      id: 4, 
+      productImage: image4,
+      name: "Seagate Portable 2TB External Hard Drive Portable HDD – USB 3.0 for PC, Mac, PlayStation, & Xbox - 1-Year Rescue Service (STGX2000400)",
+      price: 59.99,
+      shipping: 15.49
+    },
+    {
+      id: 5, 
+      productImage: image5,
+      name: "Roku Streaming Stick 4K 2021 | Streaming Device 4K/HDR/Dolby Vision with Roku Voice Remote and TV Controls",
+      price: 29.98,
+      shipping: 15.49
+    },
+    {
+      id: 6, 
+      productImage: image6,
+      name: "iPhone Charger, TAKAGI Lightning Cable 3PACK 6FT Nylon Braided USB Charging Cable High Speed Data Sync Transfer Cord Compatible with iPhone 13/12/11 Pro Max/XS MAX/XR/XS/X/8/7/Plus/6S/6/SE/5S/iPad",
+      price: 9.99,
+      shipping: 14.97
+    },
+  ]);
+
+  const search = () => {
+    console.log("clicked");
+  }
+
+  return (
+    <div> 
+      <input type="text" placeholder='Search' />
+      <button onClick={ () => search() }>Search</button>     
+      <div className='productContainer'>
+        { products.map( p => <Products name={ p.name } image={ p.productImage } price={ p.price } shipping={ p.shipping } /> ) }  
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ exercise3/src/components/Products.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Products(props){
+    return(     
+        <div className="product">
+            <img src={ props.image } />
+            <h4>{ props.name }</h4>
+            <div>${ props.price }</div>
+            <div>${ props.shipping } shipping</div>
+        </div>
+    )
+        }
